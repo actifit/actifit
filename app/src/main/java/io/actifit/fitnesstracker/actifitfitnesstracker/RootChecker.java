ec21fad817d780a0e2dc23b72007dce1012a6f14
ceruleanblue <41233245+cerulean-skies@users.noreply.github.com>
2018-08-17 14:28:12 -0400

    Update RootChecker.java

--- app/src/main/java/io/actifit/fitnesstracker/actifitfitnesstracker/RootChecker.java
+++ app/src/main/java/io/actifit/fitnesstracker/actifitfitnesstracker/RootChecker.java
@@ -1,6 +1,6 @@
 package io.actifit.fitnesstracker.actifitfitnesstracker;
 
-import android.content.Context;
+import android.content.Context; //Might be able to remove this line
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -9,34 +9,34 @@ import java.nio.Buffer;
 
 public class RootChecker {
 
-    public static boolean isDeviceRooted() {
+    public static boolean isDeviceRooted() { // Call and check each function below
         return  checkRoot1() || checkRoot2() || checkRoot3();
     }
 
-    private static boolean checkRoot1() {
-        String tags = android.os.Build.TAGS;
-        return tags != null && tags.contains("test-keys");
-    }
+    private static boolean checkRoot1() {  // One popular method of checking for root access, is to look for a Test Key.
+        String tags = android.os.Build.TAGS; // Variable containing TAGS
+        return tags != null && tags.contains("test-keys"); // 'test-keys' represent a custom signature, rather than official.
+    } // Official applications are signed with 'release-keys'.
 
-    private static boolean checkRoot2() {
+    private static boolean checkRoot2() { // Another method of checking to see if root access is available.
         String[] paths = { "/system/bin/su", "/system/app/Superuser.apk", "/data/local/su", "/sbin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
-                "/system/bin/failsafe/su", "/su/bin/su"};
-        for (String path : paths) {
-            if (new File(path).exists()) return true;
+                "/system/bin/failsafe/su", "/su/bin/su"}; //All of these paths should be locked to the average user. There may be more.
+        for (String path : paths) { // for each path in our list/collection of paths,
+            if (new File(path).exists()) return true; // return true if path can be reached.
         }
-        return false;
+        return false; // Else return false.
     }
 
-    private static boolean checkRoot3() {
-        Process process = null;
+    private static boolean checkRoot3() { // A 3rd and final method of checking for root.
+        Process process = null; //create a new proccess to run.
         try {
-            process = Runtime.getRuntime().exec(new String[] { "/system/xbin/which", "su" });
-            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            if (in.readLine() != null) return true;
+            process = Runtime.getRuntime().exec(new String[] { "/system/xbin/which", "su" }); // Newer devices may need 'sbin' in place of 'xbin'
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream())); // Buffered reader to read and store data.
+            if (in.readLine() != null) return true; // if the buffered reader has gathered data, this shows us we have root access, 
+            return false; // as the su command is restricted. Else, return false.
+        } catch (Throwable t) { 
             return false;
-        } catch (Throwable t) {
-            return false;
-        } finally {
+        } finally { // finally reset process for next use.
             if (process != null) process.destroy();
         }
     }
