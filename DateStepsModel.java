490e18c1910faf28438999820474e920a9d291f8
Mohammad Farhat <mcfarhat@gmail.com>
2018-11-30 19:19:20 +0200

    Delete DateStepsModel.java

--- DateStepsModel.java
+++
@@ -1,8 +0,0 @@
-package io.actifit.fitnesstracker.actifitfitnesstracker;
-
-public class DateStepsModel {
-
-    public String mDate;
-    public int mStepCount;
-    public String mtrackingDevice;
-}
