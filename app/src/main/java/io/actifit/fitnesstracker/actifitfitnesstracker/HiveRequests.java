aa373d456b026f4d32876e49d7a6ad43cf5195fa
Mohammad Farhat <mcfarhat@gmail.com>
2023-06-19 19:10:06 +0300

    Additional refactoring for new hive class
    Implement better handling for hive calls in an extensible approach

--- app/src/main/java/io/actifit/fitnesstracker/actifitfitnesstracker/HiveRequests.java
+++ app/src/main/java/io/actifit/fitnesstracker/actifitfitnesstracker/HiveRequests.java
@@ -2,6 +2,7 @@ package io.actifit.fitnesstracker.actifitfitnesstracker;
 
 import android.content.Context;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
@@ -15,47 +16,95 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+import static io.actifit.fitnesstracker.actifitfitnesstracker.MainActivity.TAG;
 
 public class HiveRequests {
 
     public String hiveRPCUrl;
     private Context ctx;
     private RequestQueue queue;
+    private JSONObject options;
 
     public HiveRequests(Context ctx) {
-         hiveRPCUrl = ctx.getString(R.string.hive_default_node);
-         this.ctx = ctx;
+        this.ctx = ctx;
+        //default RPC node
+        hiveRPCUrl = ctx.getString(R.string.hive_default_node);
         // Instantiate the RequestQueue.
         queue = Volley.newRequestQueue(ctx);
     }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public CompletableFuture<JSONArray> getRankedPosts(JSONObject params) {
-        CompletableFuture<JSONArray> future = new CompletableFuture<>();
-        JsonObjectRequest postsRequest;
-        try {
-            JSONObject jsonRequest = new JSONObject();
+    
+    public void setOptions(JSONObject options){
+        this.options = options;
+        if (options.has("setRPC")){
             try {
-                jsonRequest.put("jsonrpc", "2.0");
-
-                jsonRequest.put("method", ctx.getString(R.string.get_ranked_posts));
-                jsonRequest.put("params", params);
-                jsonRequest.put("id", 1);
-
+                this.hiveRPCUrl = options.getString("setRPC");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+        }
+    }
+/*
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<JSONArray> getComments(JSONObject params) {
+        JsonObjectRequest request;
 
+    }*/
+@RequiresApi(api = Build.VERSION_CODES.N)
+    public JSONArray getRankedPosts(JSONObject params){
+        JSONArray outcome = new JSONArray();
+        CompletableFuture<JSONArray> future = this.processRequest(params);
+        try {
+            JSONArray result = future.join(); // Waits for the future to complete and returns the result
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return outcome;
+            // Handle the exception
+        }
+            /*future.thenAccept(new Consumer<JSONObject>() {
+                @Override
+                public void accept(JSONObject result) {
+                    try {
+                        System.out.println("hello");
+                        JSONArray postArray = result.getJSONArray("result");
+                        return postArray;
+                        //return (JSONArray)result;
 
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            future.thenApply(result -> {
+                // Perform the desired action and return a value
+                System.out.println("Result: " + result);
+                return result;
+            }).exceptionally(error -> {
+                // Handle the error response here
+                System.out.println(error.getMessage());
+                Log.d(TAG, ">>>test:" + error.getMessage());
+                return null;
+            });*/
+            //return outcome;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<JSONArray> processRequest(JSONObject params) {
+        CompletableFuture<JSONArray> future = new CompletableFuture<>();
+        JsonObjectRequest request;
+        try {
             // Request the transactions of the user first via JsonArrayRequest
             // according to our data format
-            postsRequest = new JsonObjectRequest(Request.Method.POST,
+            request = new JsonObjectRequest(Request.Method.POST,
                     hiveRPCUrl, null, response -> {
                 try {
                     JSONArray postArray = response.getJSONArray("result");
                     future.complete(postArray);
+                    //future.complete(response);
                     //return future;
                 } catch (JSONException e) {
+                //} catch (Exception e) {
                     e.printStackTrace();
                 }
             },error -> {
@@ -64,18 +113,7 @@ public class HiveRequests {
             }){
                 @Override
                 public byte[] getBody() {
-                    JSONObject jsonRequest = new JSONObject();
-                    try {
-                        jsonRequest.put("jsonrpc", "2.0");
-
-                        jsonRequest.put("method", ctx.getString(R.string.get_ranked_posts));
-                        jsonRequest.put("params", params);
-                        jsonRequest.put("id", 1);
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    return jsonRequest.toString().getBytes();
+                    return prepareJSONReq(ctx.getString(R.string.get_ranked_posts), params);
                 }
                 @Override
                 public String getBodyContentType() {
@@ -87,7 +125,7 @@ public class HiveRequests {
             //transactionRequest.setRetryPolicy(new DefaultRetryPolicy(15000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
             // Add transaction request to be processed
-            queue.add(postsRequest);
+            queue.add(request);
 
         }catch(Exception exception){
             exception.printStackTrace();
@@ -95,6 +133,20 @@ public class HiveRequests {
         }
         //return future;
         return future;
+    }
 
+    private byte[] prepareJSONReq(String method, JSONObject params){
+        JSONObject jsonRequest = new JSONObject();
+        try {
+            jsonRequest.put("jsonrpc", "2.0");
+
+            jsonRequest.put("method", method);
+            jsonRequest.put("params", params);
+            jsonRequest.put("id", 1);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonRequest.toString().getBytes();
     }
 }
