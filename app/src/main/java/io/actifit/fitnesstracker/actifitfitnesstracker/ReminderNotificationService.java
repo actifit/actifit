2a347024f214379df0357c72193c81e650af6344
Mohammad <mcfarhat@gmail.com>
2026-01-22 16:26:15 +0200

    fix all issues causing illegal argument exception in pendingIntent popups (reminder + 5k/10k notif)

--- app/src/main/java/io/actifit/fitnesstracker/actifitfitnesstracker/ReminderNotificationService.java
+++ app/src/main/java/io/actifit/fitnesstracker/actifitfitnesstracker/ReminderNotificationService.java
@@ -46,13 +46,8 @@ public class ReminderNotificationService extends BroadcastReceiver {
         Intent notifyIntent = new Intent(context, MainActivity.class);
         notifyIntent.setAction("OPEN_MAIN_ACTIVITY");
 
-        android.app.ActivityOptions options = android.app.ActivityOptions.makeBasic();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            options.setPendingIntentBackgroundActivityStartMode(android.app.ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED);
-        }
-
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, 
-            PendingIntent.FLAG_IMMUTABLE, options.toBundle());
+            PendingIntent.FLAG_IMMUTABLE, null);
 
         //prepare notification details
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,
